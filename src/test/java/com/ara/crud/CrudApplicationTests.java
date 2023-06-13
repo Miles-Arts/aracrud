66c859684cf7dcdc4c7b238cde9408c9b132cf49
Milton Figueredo Miles <gratno@gmail.com>
2023-06-13 11:44:40 -0500

    Create List CRUD Ara - Names

---
+++ src/test/java/com/ara/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ara.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
