f804ad19868e0bc3eafd7eca4a2dc1e343f53df2
Milton Figueredo Miles <gratno@gmail.com>
2023-06-13 12:30:20 -0500

    Generated package Product proyecj Ara

---
+++ src/main/java/com/ara/crud/product/Product.java
@@ -0,0 +1,60 @@
+package com.ara.crud.product;
+
+import java.time.LocalDate;
+
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private float price;
+
+    private LocalDate fecha;
+
+    private int antiguedad;
+
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+}
