e6943a94282c3cdfd7b703eae135c13e676740bf
Milton Figueredo Miles <gratno@gmail.com>
2023-06-14 18:07:53 -0500

    Create Product respository / añadiendo @

--- src/main/java/com/ara/crud/product/ProductService.java
+++ src/main/java/com/ara/crud/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.ara.crud.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,20 +11,27 @@ import java.util.List;
 @Service
 public class ProductService {
 
+    private final ProductRepository productRepository;
 
-    @GetMapping//(path = "/")
+    //@GetMapping//(path = "/")
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
     public List<Product> getProducts() {
 
-        return List.of(
-                new Product(
-
-                        2541L,
-                        "Laptop MAC",
-                        500,
-                        LocalDate.of(2023, Month.MARCH,5),
-                        2
-
-                ));
+        return this.productRepository.findAll();
+//        return List.of(
+//                new Product(
+//
+//                        2541L,
+//                        "Laptop MAC",
+//                        500,
+//                        LocalDate.of(2023, Month.MARCH,5),
+//                        2
+//
+//                ));
     }
 
 }
